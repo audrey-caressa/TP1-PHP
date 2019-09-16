1104df46e46b666d6942786e9e0982c73c94124d
c18003981 <audrey.caressa@etu.univ-amu.fr>
2019-09-16 15:10:14 +0200

    it works

--- TP1.php
+++ TP1.php
@@ -4,6 +4,20 @@ echo 'C\'est pas mal non ? <br/>';
 $var1 = 6;
 $var2 = 1.3;
 $var3 = 'Variable 3';
-echo "$var1 + $var2";
-echo $var1 + $var2;
+echo "$var1 + $var2"; //6 + 1.3
+echo $var1 + $var2; // 7.3
+echo $var1 + $var3; // impossible
+
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> 
+            <html lang="fr">
+                <head>
+                    <title>' . PHP_EOL . $title . '</title>
+                </head>
+                <body>' . PHP_EOL
+    ;
+};
+
+start_page('titre');
 ?>
